fdb65e61218317958bd76f70adc66382efbdc91b
Sandun Jayasekara <jmsrjayasekara97@gmail.com>
2022-10-08 13:04:43 +0530

    Update about Me Section

--- myportfolio/src/components/about/About.jsx
+++ myportfolio/src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../Assets/sampling-water.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaUserFriends} from 'react-icons/fa'
+import {FaFolderMinus} from 'react-icons/fa'
 
 const About = () => {
   return (
@@ -10,9 +13,32 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="" />
+            <img src={ME} alt="About image" />
 
           </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FaAward/>
+            <h5>Experince</h5>
+            <small>No Industry Level</small>
+            </article>
+
+            <article className='about__card'>
+              <FaUserFriends/>
+            <h5>Clients</h5>
+            <small>Not Yet</small>
+            </article>
+
+            <article className='about__card'>
+              <FaFolderMinus/>
+            <h5>Projects</h5>
+            <small>5+ Projects</small>
+            </article>
+          </div>
+
         </div>
       </div>
 
